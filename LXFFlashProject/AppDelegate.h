48474df5b8cb3ec656c7b5d2aa49984d2cfe1a1d
lanxiaofeng <364225622@qq.com>
2017-02-10 13:25:45 +0800

    搭建项目文件夹目录结构

---
+++ LXFFlashProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LXFFlashProject
+//
+//  Created by lanxiaofeng on 2017/2/10.
+//  Copyright © 2017年 lanxiaofeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LXFFlashProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LXFFlashProject
+//
+//  Created by lanxiaofeng on 2017/2/10.
+//  Copyright © 2017年 lanxiaofeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
